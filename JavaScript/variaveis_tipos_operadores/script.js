7f5e85a56fb922af4e25061813d315e5a8cdc131
marcelo <marcelo1mjam@gmail.com>
2023-08-02 14:24:57 -0300

    Variáveis, tipos de dados e operadores

---
+++ JavaScript/variaveis_tipos_operadores/script.js
@@ -0,0 +1,42 @@
+let nome = "nome";
+nome = "Francisco";
+console.log(nome);
+console.log(typeof(nome))
+
+const curso = "Recode pro";
+//curso = "recode pro 2022"; Por ser constante não pode ser alterado
+console.log(curso)
+console.log(typeof(curso))
+
+const pi = 3.1416;
+console.log(pi);
+console.log(typeof(pi));
+
+let concluido = true;
+console.log(concluido);
+console.log(typeof(concluido));
+
+let num = "10";
+console.log(num*2);
+console.log(typeof(num))
+console.log(typeof(num*2))
+
+let num2 = "10,5";
+console.log(num2*2);
+console.log(typeof(num2))
+
+let num3 = 14;
+let num4 = 28;
+let num5 = "14";
+console.log(num3<num4);
+console.log(num3>num4);
+console.log(num5==num3);
+console.log(num2!=num4);
+console.log(num3 === num5);
+
+let num6 = true;
+let num7 = false;
+
+console.log(num6 == !num7)
+
+
