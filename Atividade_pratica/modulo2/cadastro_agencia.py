5c0915265c305f17a053f62fbd4d69f04e9645a1
marcelo <marcelo1mjam@gmail.com>
2023-08-06 03:02:56 -0300

    Teste código python

--- Atividade_pratica/modulo2/cadastro_agencia.py
+++ Atividade_pratica/modulo2/cadastro_agencia.py
@@ -1,30 +1,197 @@
-def cadastro_cliente (): 
+print("\nAgencia de viagens Sonhos\n");
+pessoa = [];
+destino = [];
+
+
+menu = int(input("Digite:\n 1 para cadastrar cliente\n 2 para cadastrar destino\n 3 para consultar cliente\n 4 para consultar destino\n 0 para sair:\n\n"));
+while menu != 0:
+
+    if menu == 1:
+        nome = input("\nDigite seu nome:");
+        idade = input("Digite sua idade:");
+        cpf = input("Digite seu cpf:");
+        endereco = input("Digite seu endereço:");
+        print("Cliente cadastrado com sucesso!")
         
-    nome = input("Digite seu nome:");
-    idade = input("Digite sua idade:");
-    cpf = input("Digite seu cpf:");
-    endereco = input("Digite seu endereço:");
-    pessoa = [];
-    pessoa.append(nome);
-    pessoa.append(idade);
-    pessoa.append(cpf);
-    pessoa.append(endereco);
-    consulta = input("Deseja consultar cliente: ")
+        
+        pessoa.append(nome);
+        pessoa.append(idade);
+        pessoa.append(cpf);
+        pessoa.append(endereco);
+        menu = int(input("\n\nDigite:\n 1 para cadastrar cliente\n 2 para cadastrar destino\n 3 para consultar cliente\n 4 para consultar destino\n 0 para sair:\n\n"));
+        
 
-def cadastro_destino():
-    destino = input("Digite a cidade de destino:");
-    periodo = input("Digite os dias que passará no destino: ");
-    data = input("Digite a data de saída para o destino:");
+    elif menu == 2:
+        cidade = input("Digite a cidade de destino:");
+        periodo = input("Digite os dias disponíveis para o destino:");
+        data = input("Digite a data de saída para o destino:");
+        print("Destino cadastrado com sucesso!")
 
-cadastro = int(input("Digite 1 para cadastrar cliente, 2 para cadastrar destino:"));
+        destino.append(cidade);
+        destino.append(periodo);
+        destino.append(data);
+        menu = int(input("\n\nDigite:\n 1 para cadastrar cliente\n 2 para cadastrar destino\n 3 para consultar cliente\n 4 para consultar destino\n 0 para sair:\n\n"));
 
-if (cadastro == 1):
-    cadastro_cliente();
-if (cadastro == 2):
-    cadastro_destino();
-consulta = int(input("Digite 3 para consultar cliente, 4 para consultar destino:"));
 
-if (consulta == 3):
-    print(pessoa);
-if (consulta == 4):
-    print(cadastro_destino);
+    elif menu == 3:
+        if menu == 3 and pessoa == []:
+            print("\n\nNão há clientes cadastrados, por vafor cadastre pelo menos 1 cliente!");
+            menu = int(input("\n\nDigite:\n 1 para cadastrar cliente\n 2 para cadastrar destino\n 3 para consultar cliente\n 4 para consultar destino\n 0 para sair:\n\n"));
+
+        else:
+        
+            print(pessoa);     
+            menu = int(input("\n\nDigite:\n 1 para cadastrar cliente\n 2 para cadastrar destino\n 3 para consultar cliente\n 4 para consultar destino\n 0 para sair:\n\n"));
+    
+    elif menu == 4:
+        if menu == 4 and destino == []:
+            print("\n\nNão há destinos cadastrados, por vafor cadastre pelo menos 1 destino!");
+            menu = int(input("\n\nDigite:\n 1 para cadastrar cliente\n 2 para cadastrar destino\n 3 para consultar cliente\n 4 para consultar destino\n 0 para sair:\n\n"));
+        else:
+            print(destino);
+            menu = int(input("\n\nDigite:\n 1 para cadastrar cliente\n 2 para cadastrar destino\n 3 para consultar cliente\n 4 para consultar destino\n 0 para sair:\n\n"));
+
+"""
+
+Agencia de viagens Sonhos
+
+Digite:
+ 1 para cadastrar cliente
+ 2 para cadastrar destino
+ 3 para consultar cliente
+ 4 para consultar destino
+ 0 para sair:
+
+3
+
+
+Não há clientes cadastrados, por vafor cadastre pelo menos 1 cliente!
+
+
+Digite:
+ 1 para cadastrar cliente
+ 2 para cadastrar destino
+ 3 para consultar cliente
+ 4 para consultar destino
+ 0 para sair:
+
+4
+
+
+Não há destinos cadastrados, por vafor cadastre pelo menos 1 destino!
+
+
+Digite:
+ 1 para cadastrar cliente
+ 2 para cadastrar destino
+ 3 para consultar cliente
+ 4 para consultar destino
+ 0 para sair:
+
+1
+
+Digite seu nome:Marcelo
+Digite sua idade:36
+Digite seu cpf:123.453.456-34
+Digite seu endereço:Rua Safira
+Cliente cadastrado com sucesso!
+
+
+Digite:
+ 1 para cadastrar cliente
+ 2 para cadastrar destino
+ 3 para consultar cliente
+ 4 para consultar destino
+ 0 para sair:
+
+3
+['Marcelo', '36', '123.453.456-34', 'Rua Safira']
+
+
+Digite:
+ 1 para cadastrar cliente
+ 2 para cadastrar destino
+ 3 para consultar cliente
+ 4 para consultar destino
+ 0 para sair:
+
+1
+
+Digite seu nome:Sandro
+Digite sua idade:65
+Digite seu cpf:342.345.123-23
+Digite seu endereço:Rua Carmo
+Cliente cadastrado com sucesso!
+
+
+Digite:
+ 1 para cadastrar cliente
+ 2 para cadastrar destino
+ 3 para consultar cliente
+ 4 para consultar destino
+ 0 para sair:
+
+3
+['Marcelo', '36', '123.453.456-34', 'Rua Safira', 'Sandro', '65', '342.345.123-23', 'Rua Carmo']
+
+
+Digite:
+ 1 para cadastrar cliente
+ 2 para cadastrar destino
+ 3 para consultar cliente
+ 4 para consultar destino
+ 0 para sair:
+
+2
+Digite a cidade de destino:Belo Horizonte
+Digite os dias disponíveis para o destino:4
+Digite a data de saída para o destino:12/12/2023
+Destino cadastrado com sucesso!
+
+
+Digite:
+ 1 para cadastrar cliente
+ 2 para cadastrar destino
+ 3 para consultar cliente
+ 4 para consultar destino
+ 0 para sair:
+
+4
+['Belo Horizonte', '4', '12/12/2023']
+
+
+Digite:
+ 1 para cadastrar cliente
+ 2 para cadastrar destino
+ 3 para consultar cliente
+ 4 para consultar destino
+ 0 para sair:
+
+2
+Digite a cidade de destino:Brasília
+Digite os dias disponíveis para o destino:5
+Digite a data de saída para o destino:23/04/2024
+Destino cadastrado com sucesso!
+
+
+Digite:
+ 1 para cadastrar cliente
+ 2 para cadastrar destino
+ 3 para consultar cliente
+ 4 para consultar destino
+ 0 para sair:
+
+4
+['Belo Horizonte', '4', '12/12/2023', 'Brasília', '5', '23/04/2024']
+
+
+Digite:
+ 1 para cadastrar cliente
+ 2 para cadastrar destino
+ 3 para consultar cliente
+ 4 para consultar destino
+ 0 para sair:
+
+0
+
+"""
