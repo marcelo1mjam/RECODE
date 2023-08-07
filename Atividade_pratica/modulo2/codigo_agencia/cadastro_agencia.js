803e01ec9f1e5f8be7cfd8447304f3832087a979
marcelo <marcelo1mjam@gmail.com>
2023-08-07 02:06:00 -0300

    Cadastro e consultas html/javascript

--- Atividade_pratica/modulo2/codigo_agencia/cadastro_agencia.js
+++ Atividade_pratica/modulo2/codigo_agencia/cadastro_agencia.js
@@ -1,12 +1,68 @@
-function cadastro_cliente(nome,cpf,idade,endereco){
-    const cliente = {
-        nome:nome,
-        cpf:cpf,
-        idade:idade,
-        endereco:endereco
+
+    const clientes = [];
+    
+    function cadastrarCliente() {
+
+        const cliente = {};
+    
+        const nome = document.getElementById("Nome").value;
+        const idade = document.getElementById("Idade").value;
+        const cpf = document.getElementById("CPF").value;
+        const endereco = document.getElementById("Endereco").value;
+
+        cliente.Nome = nome;
+        cliente.Idade = idade;
+        cliente.CPF = cpf;
+        cliente.Endereço = endereco;
+
+        clientes.push(cliente); 
+        
+        document.getElementById("Nome").value = "";
+        document.getElementById("Idade").value = "";
+        document.getElementById("CPF").value = "";
+        document.getElementById("Endereco").value = "";
 
     }
 
-}
-cadastro_cliente('Marcelo','1234578909', '36', 'rua Santos, 22, Belo Horizonte MG' );
-console.log(cliente);
+   
+
+    function consultarCliente(){
+        const resultadoElement = document.getElementById("resultado");
+        resultadoElement.innerText = JSON.stringify(clientes, null, 2);
+    }
+  
+    
+
+    const destinos = [];
+    
+    function cadastrarDestino() {
+
+        const destino = {};
+    
+        const cidade = document.getElementById("Cidade").value;
+        const periodo = document.getElementById("Periodo").value;
+        const data = document.getElementById("Data").value;
+       
+
+        destino.Cidade = cidade;
+        destino.Período = periodo;
+        destino.Data = data;
+        
+
+        destinos.push(destino); 
+        
+        document.getElementById("Cidade").value = "";
+        document.getElementById("Periodo").value = "";
+        document.getElementById("Data").value = "";
+        
+
+    }
+
+   
+
+    function consultarDestino(){
+        const resultadoElement = document.getElementById("resultado");
+        resultadoElement.innerText = JSON.stringify(destinos, null, 2);
+    }
+  
+    
